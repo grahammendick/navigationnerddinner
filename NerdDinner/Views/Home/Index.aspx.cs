cbfb28bc603d8a628f8a938c72f65a9f4b8ffde1
GrahamMendick <grahammendick@users.noreply.github.com>
2011-09-04 12:45:36 +0100

    Changed to match documentation

--- NerdDinner/Views/Home/Index.aspx.cs
+++ NerdDinner/Views/Home/Index.aspx.cs
@@ -15,7 +15,6 @@ namespace NerdDinner.Views.Home
 	{
 		protected void Page_Load(object sender, EventArgs e)
 		{
-			UpcomingLink.NavigateUrl = StateController.GetNavigationLink("Dinners");
 		}
 
 		[WebMethod]
